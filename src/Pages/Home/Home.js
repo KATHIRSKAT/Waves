44e72b12e2ff7f8eb76e0449ae313cca228ee017
KATHIRSKAT <kathirskat@gmail.com>
2025-03-14 15:53:00 +0530

    removed accomadation from home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -24,20 +24,7 @@ function Home() {
                 
                     </div>
 
-                    {/* Accommodation Section */}
-                    <div className="section accommodation">
-                        <h2>Accommodation</h2>
-                        <p>
-                            We have partnered with college hostels near the venue to provide discounted rates for symposium attendees. Please fill out the form below to register for accommodation.
-                        </p>
-                        <ul>
-                            <li className='accButton'>
-                                <a href="https://forms.gle/EqF8niZTUMnDbB8d6" target="_blank" rel="noopener noreferrer" className='accLink'>
-                                    Register
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                   
 
                    
 
